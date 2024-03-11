bc3329f5e08913d450c26704dab7fa397b18c476
Juan Carlos Cordovez-Mantilla <cordovez@el-cordovez.com>
2024-03-11 11:59:35 +0100

    ItemLoader

--- scrapers/guardian/guardian/items.py
+++ scrapers/guardian/guardian/items.py
@@ -1,12 +1,26 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
-import scrapy
+from scrapy import Item, Field
+from itemloaders.processors import TakeFirst, MapCompose, Join
+from w3lib.html import remove_tags
 
 
-class GuardianItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+def custom_function():
     pass
+
+
+class GuardianItem(Item):
+    headline = Field(
+        input_processor=MapCompose(remove_tags), output_processor=TakeFirst()
+    )
+    author = Field(
+        input_processor=MapCompose(remove_tags), output_processor=TakeFirst()
+    )
+    teaser = Field(
+        input_processor=MapCompose(remove_tags), output_processor=TakeFirst()
+    )
+    published = Field(
+        input_processor=MapCompose(remove_tags), output_processor=TakeFirst()
+    )
+    content = Field(
+        input_processor=MapCompose(remove_tags, custom_function),
+        output_processor=Join(),
+    )
--- scrapers/guardian/guardian/spiders/opinions.py
+++ scrapers/guardian/guardian/spiders/opinions.py
@@ -2,6 +2,9 @@ import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from datetime import datetime
+from ..items import GuardianItem
+from scrapy.loader import ItemLoader
+
 
 month = datetime.now().strftime("%b").lower()
 year = datetime.now().strftime("%Y")
@@ -10,9 +13,32 @@ year = datetime.now().strftime("%Y")
 class OpinionsSpider(CrawlSpider):
     name = "opinions"
     allowed_domains = ["theguardian.com"]
-    start_urls = ["https://www.theguardian.com/uk/commentisfree"]
+    start_urls = ["https://www.theguardian.com/uk/commentisfree/"]
 
-    rules = Rule(LinkExtractor(allow=(r"{year}/{month}")))
+    rules = (
+        Rule(LinkExtractor(allow=(rf"uk/commentisfree/",), deny=(r"-cartoon",))),
+        Rule(
+            LinkExtractor(allow=(rf"{year}/{month}/",)),
+            callback="parse_opinion",
+        ),
+    )
 
-    def parse(self, response):
-        pass
+    def parse_opinion(self, response):
+        """headline = response.css("h1::text").get()
+        'Germany’s reputation for decisive leadership is in tatters when Europe needs it most'
+        author = response.css("div.dcr-0 a::text").get()
+        'Simon Tisdall'
+        teaser= response.css("div.dcr-1kpcv08::text").get()
+        response.css(".dcr-1qp23oo p::text").get()
+        'Olaf Scholz’s endless dithering over Ukraine is playing into Putin’s hands'
+        published = response.css("div.dcr-1kpcv08 span::text").get()
+        'Sat 9 Mar 2024 20.07 CET'
+        content= text_combined = ''.join(response.css("#maincontent p::text, #maincontent span::text").getall())
+        """
+        guardian_article = ItemLoader(opinion=GuardianItem(), response=response)
+        guardian_article.add_css("headline", "h1fd")
+        guardian_article.add_css("author", ".dcr-0 a")
+        guardian_article.add_css("teaser", ".dcr-1qp23oo p")
+        guardian_article.add_css("published", ".dcr-1kpcv08 span")
+        guardian_article.add_css("content", "#maincontent p, #maincontent span ")
+        return guardian_article.load_item()
